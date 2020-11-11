8227f58b87055b29efd6fa00177eb71748c16c1f
LABOBMED-DH03\acox <andres-cox>
2020-11-11 11:43:43 -0400

    this is a small example of the python compiler

---
+++ server_file.py
@@ -0,0 +1,15 @@
+# import client_file
+# import client_input_output
+
+import os
+# files = os.listdir('.')
+
+# FOR FINDING FILES
+# for file in files:
+#     if '.py' in file:
+#         print (file)
+
+os.system('python client_files/client_file.py')
+os.system('python --version')
+
+print("this is server file")
