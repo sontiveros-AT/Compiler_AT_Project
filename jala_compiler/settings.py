4d1f081e878b2c3a05a80b2862a88a80107e4066
Gonzalo.Alarcon <gonzalo.alarcon@jala-foundation.org>
2020-11-12 14:56:04 -0400

    Settings was changed to mysql connection.

--- jala_compiler/settings.py
+++ jala_compiler/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'code_editor',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'jala_compiler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jalacompilerdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
@@ -118,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# NEW GONZALOALARCON
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
+MEDIA_URL = "/storage/"
