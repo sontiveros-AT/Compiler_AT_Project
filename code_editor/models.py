c0171af2aedd97cacf0561331bd1063d6e4dfa4c
GonzaloIvanAlarconRoldan <74021190+GonzaloIvanAlarconRoldan@users.noreply.github.com>
2020-11-12 16:58:36 -0400

    Update models.py
    change copyright file name

--- code_editor/models.py
+++ code_editor/models.py
@@ -1,5 +1,5 @@
 #
-# @javacompiler.py Copyright (c) 2020 Jalasoft.
+# @models.py Copyright (c) 2020 Jalasoft.
 # 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
 # 1376 subsuelo Edif. La Unión, Av. Gral. Inofuentes, Calacoto, La Paz, Bolivia
 # All rights reserved.
