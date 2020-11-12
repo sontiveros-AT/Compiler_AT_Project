ca339f9d217f251036c673dae404ea3b7c3c13ca
Gonzalo.Alarcon <gonzalo.alarcon@jala-foundation.org>
2020-11-12 15:00:05 -0400

    Instance all table classes in models.py

--- code_editor/models.py
+++ code_editor/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+from code_editor.model_languages import Languages
+from code_editor.model_projects import Projects
+from code_editor.model_files import Files
