f8af040f21f3c04ee1d3e3db44d6d282a47bffa6
Juan Sebastian Ontiveros Gonzales <sebastian.ontiveros@jala-foundation.org>
2020-11-12 12:57:31 -0400

    Created view hanlding for code_editor demo

--- code_editor/views.py
+++ code_editor/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from Core.compile import Compiler
+from Core.compile.parameters import Parameters
+
+
+class CodeEditorView(TemplateView):
+    template_name = 'code_editor/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        # establish file and language for params
+        params = Parameters()
+        params.set_language('python')
+        params.set_file(r'../media/python/client_files/client_file.py')
+
+        # execute file
+        program = Compiler(params)
+        output = program.execute()
+        return render(request, self.template_name, {'output': output})
