7c7a58ced0fdc87a5c386524f935898683e7b792
Juan Sebastian Ontiveros Gonzales <sebastian.ontiveros@jala-foundation.org>
2020-11-18 21:34:29 -0400

    Added Python compilation implementation

---
+++ code_editor/core/python/python_executor.py
@@ -0,0 +1,24 @@
+#
+# @python_executor.py Copyright (c) 2020 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# 1376 subsuelo Edif. La Unión, Av. Gral. Inofuentes, Calacoto, La Paz, Bolivia
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the termns of the license agreement you entered into
+# with Jalasoft.
+#
+
+import subprocess
+from code_editor.core.executor import Executor
+
+
+class PythonExecutor(Executor):
+    def run(self, cmd):
+        output = subprocess.run(cmd, capture_output=True, text=True)
+        if output.returncode != 0:
+            return output.stderr
+
+        return output.stdout
