2f4abcd8bb7cdf7aa744786492a9ad0e94448e89
Juan Sebastian Ontiveros Gonzales <sebastian.ontiveros@jala-foundation.org>
2020-11-18 21:36:07 -0400

    Added compilation facade class and settings needed

---
+++ code_editor/core/settings.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+
+PYTHON39_PATH = settings.BASE_DIR / 'third_parties/python/Python39-32/python.exe'
+JAVA13_PATH = settings.BASE_DIR / 'third_parties/java/jdk-13.0.2/bin'
+JAVA_FILES = settings.BASE_DIR / 'media/java/java_files'
